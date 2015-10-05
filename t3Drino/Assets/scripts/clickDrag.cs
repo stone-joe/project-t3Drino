88f4cd09849b6ab3334a5aa630156e33e7b1f198
unknown <lamribenn@gmail.com>
2015-10-05 16:43:47 -0400

    v1 tetrins spawn fall and can be grabbed

---
+++ t3Drino/Assets/scripts/clickDrag.cs
@@ -0,0 +1,37 @@
+﻿//nabil click and drag 
+//this is just a test
+using UnityEngine;
+using System.Collections;
+
+public class clickDrag : MonoBehaviour {
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    public GameObject wholeobject;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.parent.transform.position = curPosition; //I make the parent move to the ouse's position.
+
+    }
+
+}
---
+++ t3Drino/Assets/scripts/spawner.cs
@@ -0,0 +1,45 @@
+﻿//Nabil spawner
+//this is just a test
+using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+    //this script is attached to an empty GameObject called TheSpawner
+    // if you click on TheSpawner in the Hierarchy column you will see this script as 
+    // a component of TheSpawner in the Innspector column
+
+    // created an array of GameObjects , since this array is public
+    //you can see it in the  inspector
+    // I dragged my tetrino prefabs in the array and we now have  ref to each one of the 
+    public GameObject[] golist = new GameObject[6];
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+    private float nextActionTime = 0.0f;
+    public float period = 3f; //every 3 seconds
+    private int index = 0;    
+    private int tetrinoCount = 0;
+    void FixedUpdate()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+
+
+            tetrinoCount++;
+            if (tetrinoCount < 12)
+            {
+                index = tetrinoCount % 5;
+                GameObject go = golist[index];
+                Instantiate(go, transform.position, transform.rotation); //this will instanciate a tetrino at the location and rotation of the object on which this script is attached (TheSpawner)
+            }
+
+
+        }
+    }
+}
