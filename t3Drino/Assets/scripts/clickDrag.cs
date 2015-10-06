85f0ee4b734fc515d15ece2f1e8c59df98c7b3ef
unknown <lamribenn@gmail.com>
2015-10-06 19:30:56 -0400

    implemented rotate in clickDrag

--- t3Drino/Assets/scripts/clickDrag.cs
+++ t3Drino/Assets/scripts/clickDrag.cs
@@ -6,43 +6,55 @@ using System.Collections;
 public class clickDrag : MonoBehaviour {
     private Vector3 screenPoint;
     private Vector3 offset;
+    private Vector3 curPosition;
+    private bool canRotate;
+    private float rortateSpeed;
 
     public GameObject wholeobject;
 
+
     // Use this for initialization
-    void Start () {
-    
+    void Start ()
+    {
+        canRotate = false;
+        rortateSpeed = 300.0f;
     }
     
     // Update is called once per frame
-    void Update () {
-    
+    void FixedUpdate ()
+    {
+        if (canRotate  && transform.tag == "movableTag") {
+            transform.RotateAround(curPosition, Vector3.back, rortateSpeed * Time.deltaTime  * Input.GetAxis("Horizontal"));
+        }
     }
-
 
     void OnMouseDown()
     {
+        canRotate = true;
         screenPoint = Camera.main.WorldToScreenPoint(transform.position);
         offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseUp()
+    {
+        canRotate = false;
     }
 
     void OnMouseDrag()
     {
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+         curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
         if(transform.tag == "movableTag") {
-            transform.position = curPosition; //I make the parent move to the ouse's position.
+            transform.position = curPosition; //I make the parent move to the mouse's position.
         }
     }
     
     void OnCollisionEnter(Collision collision)
     {
-        //if (collision.gameObject.CompareTag ("floorTag") || collision.gameObject.CompareTag ("notMovableTag")) {
-            foreach (Transform child in transform) {
-                child.GetComponent<Renderer> ().material.color = Color.black;
-            }
-            transform.tag = "notMovableTag";
-        //}
+        foreach (Transform child in transform) {
+            child.GetComponent<Renderer> ().material.color = Color.black;
+        }
+        transform.tag = "notMovableTag";
     }
 
 }
