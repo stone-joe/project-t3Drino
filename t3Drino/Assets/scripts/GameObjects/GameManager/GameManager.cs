1bc0ae5803c9b3afd24d8755b35b27bd0e03afd8
Jeannie <jetrinh33@gmail.com>
2015-10-08 00:21:41 -0400

    Add GameManager class to set up levels and player scores, etc. Loader class to initialize an instance of GameManager that persists for entire game

---
+++ t3Drino/Assets/scripts/GameObjects/GameManager/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	private int _level = 1;
+	//public BoardManager boardScript; // For referecing later
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else
+			Destroy (gameObject);
+
+		DontDestroyOnLoad(gameObject); // Allows persist between scenes
+
+		//boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	public void InitGame()
+	{
+		//boardScript.SetupScene(_level);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
