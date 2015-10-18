3cbdf3f623a6f16ffde7daeee1f87cc6c8f3c00e
Jeannie <jetrinh33@gmail.com>
2015-10-18 03:33:47 -0400

    Clean up TetrinoPreviewer code.

--- t3Drino/Assets/scripts/GameObjects/TetrinoQueue/TetrinoPreviewer.cs
+++ t3Drino/Assets/scripts/GameObjects/TetrinoQueue/TetrinoPreviewer.cs
@@ -12,17 +12,41 @@ public class TetrinoPreviewer : MonoBehaviour {
 	private Texture2D _nextTetrinoSprite;
 	private int _totalSprites;
 	private int I, J, L, O, S, T, Z;
+	private float _scaleWidth;
+	private float _scaleHeight;
 
 	// Use this for initialization
 	void Start () {
+		
+		LoadTetrinoSelectorReference();					// Load selector/queue
+		LoadTextureArrayIndices();
+		LoadTextures();
+		InitializeNextTetrominoGO();
+		SetNextTetrominoLocation(16.49f, 12.81f, 0);	// Set previewer location
+	}
 
-		// Load selector/queue
+
+	void Update () {
+
+		SetNextTetromino();
+
+		// Change texture and rescale
+		NextTetrino.GetComponent<Renderer>().material.mainTexture = _nextTetrinoSprite;
+		NextTetrino.GetComponent<Transform>().localScale = new Vector3(_nextTetrinoSprite.width/_scaleWidth,
+		                                                               _nextTetrinoSprite.height/_scaleHeight,
+		                                                               1);
+	}
+
+	private void LoadTetrinoSelectorReference()
+	{
 		GameObject go = GameObject.Find("TetrinoSelector");
 		_tetrinoSelector = (TetrinoSelector) go.GetComponent(typeof(TetrinoSelector));
+	}
 
-		// Load texture array indices
+	private void LoadTextureArrayIndices()
+	{
 		_totalSprites = 7;
-
+		
 		I = 0;
 		J = 1;
 		L = 2;
@@ -30,8 +54,10 @@ public class TetrinoPreviewer : MonoBehaviour {
 		S = 4;
 		T = 5;
 		Z = 6;
+	}
 
-		// Load textures
+	private void LoadTextures()
+	{
 		_tetrinoSprites = new Texture2D[_totalSprites];
 		_tetrinoSprites[I] = (Texture2D)UnityEditor.AssetDatabase.LoadAssetAtPath(TetrinoSpriteLibrary.PathTetrominoSpriteI, typeof(Texture2D));
 		_tetrinoSprites[J] = (Texture2D)UnityEditor.AssetDatabase.LoadAssetAtPath(TetrinoSpriteLibrary.PathTetrominoSpriteJ, typeof(Texture2D));
@@ -40,78 +66,73 @@ public class TetrinoPreviewer : MonoBehaviour {
 		_tetrinoSprites[S] = (Texture2D)UnityEditor.AssetDatabase.LoadAssetAtPath(TetrinoSpriteLibrary.PathTetrominoSpriteS, typeof(Texture2D));
 		_tetrinoSprites[T] = (Texture2D)UnityEditor.AssetDatabase.LoadAssetAtPath(TetrinoSpriteLibrary.PathTetrominoSpriteT, typeof(Texture2D));
 		_tetrinoSprites[Z] = (Texture2D)UnityEditor.AssetDatabase.LoadAssetAtPath(TetrinoSpriteLibrary.PathTetrominoSpriteZ, typeof(Texture2D));
+	}
 
+	private void InitializeNextTetrominoGO()
+	{
 		NextTetrino = GameObject.CreatePrimitive(PrimitiveType.Quad);							// Create base game object of type Quad
 		NextTetrino.name = "NextTetrino";														// Name base game object
 		Destroy(NextTetrino.GetComponent("MeshCollider"));										// Remove collider
 		NextTetrino.GetComponent<Renderer>().material.shader = Shader.Find("Sprites/Diffuse");	// Set transparency
-
-		// Set location
-		NextTetrino.transform.position = new Vector3(16.49f, 12.81f, 0);
 	}
 
+	private void SetNextTetrominoLocation(float x, float y, float z)
+	{
+		NextTetrino.transform.position = new Vector3(x, y, z);
+	}
 
-	void Update () {
-
-		float scaleWidth = 1;
-		float scaleHeight = 1;
-
+	private void SetNextTetromino()
+	{
 		float widthIO = 15f;
 		float heightIO = widthIO;
-
+		
 		float widthJLSTZ = 10f;
 		float heightJLSTZ = widthJLSTZ * 1.5f;
 
 		// Set texture and scaling
 		string tetrinoName = _tetrinoSelector.Queue[0].name;
-
+		
 		if (tetrinoName.Equals(TetrinoSpriteLibrary.PrefabTetrominoNameI))
 		{
 			_nextTetrinoSprite = _tetrinoSprites[I];
-			scaleWidth = widthIO;
-			scaleHeight = heightIO;
+			_scaleWidth = widthIO;
+			_scaleHeight = heightIO;
 		}
 		else if (tetrinoName.Equals(TetrinoSpriteLibrary.PrefabTetrominoNameJ))
 		{
 			_nextTetrinoSprite = _tetrinoSprites[J];
-			scaleWidth = widthJLSTZ;
-			scaleHeight = heightJLSTZ;
+			_scaleWidth = widthJLSTZ;
+			_scaleHeight = heightJLSTZ;
 		}
 		else if (tetrinoName.Equals(TetrinoSpriteLibrary.PrefabTetrominoNameL))
 		{
 			_nextTetrinoSprite = _tetrinoSprites[L];
-			scaleWidth = widthJLSTZ;
-			scaleHeight = heightJLSTZ;
+			_scaleWidth = widthJLSTZ;
+			_scaleHeight = heightJLSTZ;
 		}
 		else if (tetrinoName.Equals(TetrinoSpriteLibrary.PrefabTetrominoNameO))
 		{
 			_nextTetrinoSprite = _tetrinoSprites[O];
-			scaleWidth = widthIO;
-			scaleHeight = heightIO;
+			_scaleWidth = widthIO;
+			_scaleHeight = heightIO;
 		}
 		else if (tetrinoName.Equals(TetrinoSpriteLibrary.PrefabTetrominoNameS))
 		{
 			_nextTetrinoSprite = _tetrinoSprites[S];
-			scaleWidth = widthJLSTZ;
-			scaleHeight = heightJLSTZ;
+			_scaleWidth = widthJLSTZ;
+			_scaleHeight = heightJLSTZ;
 		}
 		else if (tetrinoName.Equals(TetrinoSpriteLibrary.PrefabTetrominoNameT))
 		{
 			_nextTetrinoSprite = _tetrinoSprites[T];
-			scaleWidth = widthJLSTZ;
-			scaleHeight = heightJLSTZ;
+			_scaleWidth = widthJLSTZ;
+			_scaleHeight = heightJLSTZ;
 		}
 		else if (tetrinoName.Equals(TetrinoSpriteLibrary.PrefabTetrominoNameZ))
 		{
 			_nextTetrinoSprite = _tetrinoSprites[Z];
-			scaleWidth = widthJLSTZ;
-			scaleHeight = heightJLSTZ;
+			_scaleWidth = widthJLSTZ;
+			_scaleHeight = heightJLSTZ;
 		}
-
-		// Change texture and rescale
-		NextTetrino.GetComponent<Renderer>().material.mainTexture = _nextTetrinoSprite;
-		NextTetrino.GetComponent<Transform>().localScale = new Vector3(_nextTetrinoSprite.width/scaleWidth,
-		                                                               _nextTetrinoSprite.height/scaleHeight,
-		                                                               1);
 	}
 }
